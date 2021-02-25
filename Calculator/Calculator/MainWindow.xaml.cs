ad289d9754fe39b993dc6a7474d442af7ce1d7cf
Danil <leonardo147wot@gmail.com>
2021-02-25 15:56:48 +0300

    Start tutorial

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in NewName.Children)
+            {
+                if (el is Button)
+                    ((Button)el).Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string s = ((Button)e.OriginalSource).Content.ToString();
+            TextBlock1.Text += s;
+        }
+    }
+}
--- WpfAppTutorial/WpfAppTutorial/MainWindow.xaml.cs
+++ WpfAppTutorial/WpfAppTutorial/MainWindow.xaml.cs
@@ -13,16 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfAppTutorial
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
-            //InitializeComponent();
+            InitializeComponent();
         }
     }
 }
